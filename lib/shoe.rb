fc4e10a28fadfe1a3875160cb5c63d35e0f2172b
Johnny Soong <johnnysoong@hotmail.com>
2018-08-07 16:54:22 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -12,37 +12,3 @@ class Shoe
     @color = color
   end
   
-  def color
-    @color
-  end
-  
-  def size=(size)
-    @size = size
-  end
-  
-  def size
-    @size
-  end
-  
-  def material=(material)
-    @material = material
-  end
-  
-  def material
-    @material
-  end
-  
-  def condition=(condition)
-    @condition = condition
-  end
-  
-  def condition
-    @condition
-  end
-  
-  def cobble
-    puts "Your shoe is as good as new!"
-  end
-  
-end
-
